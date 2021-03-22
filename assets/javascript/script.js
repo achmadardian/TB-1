b9e930d5cda6055b916c144c7a75d8e5a7ea2557
achmadardian <achmadardian37@gmail.com>
2021-03-22 14:04:31 +0700

    ubah parameter api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,12 +11,13 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
+
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Cuaca di " + name;
+    document.querySelector(".city").innerText = "Keadaan cuaca di " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
