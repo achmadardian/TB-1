265d8a8f38be7f8082600da30d16a3c6e9d1f915
achmadardian <achmadardian37@gmail.com>
2021-03-22 14:25:00 +0700

    menambah parameter temp

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,12 +17,15 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
+    const { temp_min, temp_max } = data.main;
     document.querySelector(".city").innerText = "Keadaan cuaca di " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText = "Kelembaban: " + humidity + "%";
     document.querySelector(".wind").innerText = "Kecepatan angin: " + speed + " km/h";
+    document.querySelector(".temp-min").innerText = "Temperature minimal: " + temp_min + "°C";
+    document.querySelector(".temp-max").innerText = "Temperature maximal: " + temp_max + "°C";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
